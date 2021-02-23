2c749b36760f781fe170ef6b6159607f6850e065
Bruna Calheiros <calheiros.bruna@gmail.com>
2021-02-23 15:52:25 -0300

    src/styles/components/CompletedChallenges.module.css

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,12 +1,16 @@
-import styles from '../styles/components/Profile.modules.css';
+import styles from '../styles/components/Profile.module.css';
 
 export function Profile() {
   return (
     <div className= {styles.profileContainer}>
-      <img src="https://avatars.githubusercontent.com/u/5524114?s=60&v=4://github.com/bruna15lima/bruna.png" alt="bruna15lima"/>
+      <img src="https://media-exp1.licdn.com/dms/image/C5103AQHIxchsYuYHaA/profile-displayphoto-shrink_200_200/0/1517571826663?e=1619654400&v=beta&t=vH9KdLHcg7wHd8jHENVcxvXZ2rWa4oIGnkAMpgGuZS4"/>
       <div>
         <strong>Bruna Calheiros</strong>
-        <p>Level 1</p>
+        <p>
+          <img src="icons/level.svg" alt="Level"/>
+          Level 1
+
+        </p>
       </div>
     </div>
   )
