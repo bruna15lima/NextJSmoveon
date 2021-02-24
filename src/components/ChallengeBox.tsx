1b4d69e79488da40fc59c478920e5b01436a3f43
Bruna Calheiros <calheiros.bruna@gmail.com>
2021-02-24 16:41:01 -0300

    ChallengesContext

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -1,13 +1,42 @@
+import { ChallengesContext } from '../contexts/ChallengeContext';
 import styles from '../styles/components/ChallengeBox.module.css';
 
 
 export function ChallengeBox(){
+
+  const contextData = useContext(ChallengesContext);
+
+  console.log(contextData)
   const hasActiveChallenge = true;
 
   return (
     <div className={styles.challengeBoxContainer}>
       { hasActiveChallenge ? (
-        <div/>
+        <div className={styles.challengeActive}>
+          <header>Ganhe 400xp</header>
+
+          <main>
+            <img src="icons/body.svg"/>
+            <strong>Novo desafio</strong>
+            <p>Levante e faça uma caminhada de 3 minutos.</p>
+          </main>
+        
+          <footer>
+            <button 
+              type="button"
+              className={styles.challengeFaieldButton}              
+              >
+                Falhei
+            </button>
+
+            <button 
+              type="button"
+              className={styles.challengeSucceededButton}  
+            >
+              Completei
+              </button>
+          </footer>
+        </div>
       ) : (
         <div className={styles.challengeNotActive}>
           <strong>Finalize um ciclo para receber um desafio</strong> 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,13 @@
 import '../styles/global.css'
 
+import { ChallengesContext  } from '../contexts/ChallengeContext';
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChallengesContext.Provider value={ {level:1} }>
+    <Component {...pageProps}/>
+    </ChallengesContext.Provider>
+  ) 
 }
 
 export default MyApp
