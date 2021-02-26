f03879bfabd615a872280b13fc34bd13478547fd
Bruna Calheiros <calheiros.bruna@gmail.com>
2021-02-26 12:59:48 -0300

    public/icons/avatar.svg

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -6,7 +6,7 @@ export function Profile() {
   const { level } =  useContext(ChallengesContext)
   return (
     <div className= {styles.profileContainer}>
-      <img src="https://media-exp1.licdn.com/dms/image/C5103AQHIxchsYuYHaA/profile-displayphoto-shrink_200_200/0/1517571826663?e=1619654400&v=beta&t=vH9KdLHcg7wHd8jHENVcxvXZ2rWa4oIGnkAMpgGuZS4"/>
+      <img src="icons/avatar.svg"/>
       <div>
         <strong>Bruna Calheiros</strong>
         <p>
